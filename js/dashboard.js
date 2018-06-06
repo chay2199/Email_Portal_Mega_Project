2f90790ddbd33ef8dfcfd21f37863132317642cc
Yash <ygyashgoyal@gmail.com>
2018-06-07 02:50:49 +0530

    side_navbar_dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,12 @@
 $("body").css("min-height",$(window).height());
+$(".side-nav").css("min-height",$(window).height());
+$(".side-options").css("min-width",$(".col-lg-2").width());
+
+
+
+$(".side-options").click(function(){
+	$(".side-option-group .side-options").removeClass("selected");
+	$(this).addClass("selected");
+
+
+});
