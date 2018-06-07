7f82b9588c2ee5bd58b7d5d8acfbf2fc619acece
Yash <ygyashgoyal@gmail.com>
2018-06-07 17:04:46 +0530

    dashboard_corrections

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,9 @@
-$("body").css("min-height",$(window).height());
-$(".side-nav").css("min-height",$(window).height());
+var exheight = 100 + "px" ;
+$("html").css("height",$(window).height());
+$("body").css("height","91%");
+/*$(".side-nav").css("min-height",$(window).height());*/
 $(".side-options").css("min-width",$(".col-lg-2").width());
+
 
 
 
